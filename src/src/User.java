4bd2355e47cb2c400950265afd869ee81b98935b
Michael Loney <michael.loney137@gmail.com>
2018-11-30 15:31:32 +0000

    Added User and Librarian Java objects. Added Default window that should be used as the base of most windows

---
+++ src/src/User.java
@@ -0,0 +1,94 @@
+package src;
+
+import javafx.scene.image.Image;
+
+public class User {
+
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String addressLine;
+    private String postcode;
+    private String phoneNumber;
+    private Image profileImage;
+    private int fineBalance;
+
+    public User(){
+
+    }
+
+    public User(String username, String firstName, String lastName, String addressLine, String postcode, String phoneNumber, Image profileImage, int fineBalance) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.addressLine = addressLine;
+        this.postcode = postcode;
+        this.phoneNumber = phoneNumber;
+        this.profileImage = profileImage;
+        this.fineBalance = fineBalance;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddressLine() {
+        return addressLine;
+    }
+
+    public void setAddressLine(String addressLine) {
+        this.addressLine = addressLine;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Image getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(Image profileImage) {
+        this.profileImage = profileImage;
+    }
+
+    public int getFineBalance() {
+        return fineBalance;
+    }
+
+    public void setFineBalance(int fineBalance) {
+        this.fineBalance = fineBalance;
+    }
+}
