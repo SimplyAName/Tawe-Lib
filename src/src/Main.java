f2581c9ab527d3eea02e4136e7f06099a8a08746
Michael Loney <michael.loney137@gmail.com>
2018-11-30 21:12:53 +0000

    Adding some unversioned files and setting up some other classes to be filled in later.

---
+++ src/src/Main.java
@@ -0,0 +1,19 @@
+package src;
+
+import javafx.application.Application;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+
+
+    }
+}
