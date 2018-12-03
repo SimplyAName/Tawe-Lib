1158927ab7a3286ddf17a86c8d27eafc0c80402d
SimplyAName <michael.loney137@gmail.com>
2018-12-03 18:53:22 +0000

    Set up for creating scene switching for the program

--- src/src/LibrarianInterfaceController.java
+++ src/src/LibrarianInterfaceController.java
@@ -1,7 +1,17 @@
 package src;
 
-public class LibrarianInterfaceController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LibrarianInterfaceController implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
 
 
 
+    }
 }
