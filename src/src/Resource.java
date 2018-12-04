2e436ff6e7e2d4ee8e56924c6f829d11b9828ac4
908169 <908169@ISS-P1210334217.tawe.swan.ac.uk>
2018-12-04 15:49:56 +0000

    Added javadoc

--- src/src/Resource.java
+++ src/src/Resource.java
@@ -1,8 +1,19 @@
-
+/**
+ * This class is the superclass of the resources.
+ * @author 908169
+ * @version 1
+ */
 public class Resource {
 	private int rID,year;
 	private String title,rImg;
 	
+	/**
+	 * Constructor for the class
+	 * @param rID
+	 * @param title
+	 * @param year
+	 * @param rImg
+	 */
 	public Resource(int rID,String title, int year, String rImg){
 		this.rID = rID;
 		this.title = title;
@@ -10,18 +21,34 @@ public class Resource {
 		this.rImg = rImg;
 	}
 	
+	/**
+	 * returns the resource ID
+	 * @return rID
+	 */
 	public int getID() {
 		return rID;
 	}
 	
+	/**
+	 * returns the title
+	 * @return title
+	 */
 	public String getTitle() {
 		return title;
 	}
 	
+	/**
+	 * returns the year
+	 * @return year
+	 */
 	public int getYear() {
 		return year;
 	}
 	
+	/**
+	 * gets the image
+	 * @return rImg
+	 */
 	public String getImage() {
 		return rImg;
 	}
