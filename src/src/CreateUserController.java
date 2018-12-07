67a1f1770b964f790a62b39715184c4173003654
JakeArcher95 <45101041+JakeArcher95@users.noreply.github.com>
2018-12-07 19:21:33 +0000

    Update CreateUserController.java

--- src/src/CreateUserController.java
+++ src/src/CreateUserController.java
@@ -1,21 +1,67 @@
 package src;
 
+import java.sql.SQLException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
 public class CreateUserController {
+	@FXML private TextField firstName;
+	@FXML private TextField lastName;
+	@FXML private TextField eAddress;
+	@FXML private TextField enterUsername;
+	@FXML private TextField ePostCode;
+	@FXML private TextField phoneNumber;
+	@FXML private AnchorPane CreateUserPane;
+	
+	private String fName;
+	private String lName;
+	private String address;
+	private String username;
+	private String postCode;
+	private String phone;
+	private int balance;
+	
+	private String nameQuery = "select username from user_tbl where username = " + enterUsername.getText() + ";";
+	private String addUserQuery;
+	private String imagelocation;
+	
+	
+	@FXML private void addUser() throws IllegalArgumentException, SQLException{
+		fName = firstName.getText();
+		lName = lastName.getText();
+		address = eAddress.getText();
+		postCode = ePostCode.getText();
+		phone = phoneNumber.getText();
+		balance = 0;
+		imagelocation = "";
+		
+		//checks if the username is already taken
+		if (Database.query(nameQuery) == null) {
+			username = enterUsername.getText();
+		} else {
+			;
+		}
+		
 
-    private boolean addUser(){
+		try{
+		addUserQuery = (" insert into user_tbl (username, firstnames, lastname, addrline1, postcode, phone, imagelocation, balance)"
+				+ " values (" + fName + " ," + lName + " ," + address + " ," + postCode + " ," + imagelocation + " ," + balance + " );" );
+		}
+		
+		catch (Exception e1) {
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.showAndWait();
+			
+		}
+		
+	}
 
-        //This will be the main function called when adding the user to the system.
 
-        return true;
-
-    }
-
-    private boolean checkUsername(String username){
-
-        //This will check there are no duplicate names in the database
-
-        return true;
-
-    }
 
 }
