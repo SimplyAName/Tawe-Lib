7b3596df52d7d170e4f3dba13cbb951091920497
James <jamesyjkim12@gmail.com>
2018-12-08 21:43:29 +0000

    IssueResource Created

--- src/src/CustomDrawing.java
+++ src/src/CustomDrawing.java
@@ -1,3 +1,4 @@
+package src;
 //package application;
 package src;
 
--- src/src/LibrarySearch.java
+++ src/src/LibrarySearch.java
@@ -17,26 +17,8 @@ public class LibrarySearch {
 
             }
 
-            public int borrowResource(Book, DVD, Laptop Resource;) throws Exception {
-                ResultSet rs = Database.query("SELECT * user_tbl WHERE username =  " +
-                        "" +
-                        "duedate");
-                PreparedStatement ps = null;
 
-            }
 
-            public int returnResource(Book, DVD, Laptop Resource;) throws Exception {
-                ResultSet rs = Database.query("SELECT * FROM out_tbl WHERE username = ‘' AND copyid = '' "+
-                        "INSERT INTO historic_tbl VALUES (NULL, copy_id, date from, NOW(), username");
-                PreparedStatement ps = null;
-
-            }
-
-            public int reservedCopy(Book, DVD, Laptop Resource;) throws Exception {
-                ResultSet rs = Database.query("SELECT * FROM request_tbl, copy_tbl WHERE request_tbl.copyid = copy_tbl.requestid " +
-                        "AND resource copyid = ENTER_HERE;\n");
-                PreparedStatement ps = null;
-            }
 
     }
 
