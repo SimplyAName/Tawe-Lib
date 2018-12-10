f41178b0c99b294f1320472b80d224d3dc94168f
KarlOdukwe <45237187+KarlOdukwe@users.noreply.github.com>
2018-12-10 09:14:56 +0000

    Update OverdueCopy.java
    Updated javadoc

--- src/main/OverdueCopy.java
+++ src/main/OverdueCopy.java
@@ -1,5 +1,10 @@
 package main;
 
+/**
+ * Handles the propeerties to deal with an overdue resrouce
+ * @author 
+ *
+ */
 public class OverdueCopy {
 
     private int copyid;
@@ -10,51 +15,85 @@ public class OverdueCopy {
     private String resourceName;
     private String resourceType;
 
+    
+    /**
+     * 
+     * @return Returns name of the resource
+     */
     public String getResourceName() {
         return resourceName;
     }
 
+    /**
+     * 
+     * @param Sets the name of the resource
+     */
     public void setResourceName(String resourceName) {
         this.resourceName = resourceName;
     }
 
+    /**
+     * 
+     * @return Returns the type of resource
+     */
     public String getResourceType() {
         return resourceType;
     }
 
+    
+    /**
+     * 
+     * @param Sets the resource type
+     */
     public void setResourceType(String resourceType) {
         this.resourceType = resourceType;
     }
 
+    
+    /**
+     * 
+     * @return Returns resource copy ID
+     */
     public int getCopyid() {
         return copyid;
     }
 
+    
+    /**
+     * 
+     * @param copyid sets the resource copy ID
+     */
     public void setCopyid(int copyid) {
         this.copyid = copyid;
     }
 
+    
+    
+    /**
+     * 
+     * @return Returns the date the resource was taken out
+     */
     public String getDateTakenOut() {
         return dateTakenOut;
     }
 
+    
+    /**
+     * 
+     * @param dateTakenOut Sets the date the resource was taken out
+     */
     public void setDateTakenOut(String dateTakenOut) {
         this.dateTakenOut = dateTakenOut;
     }
 
+    
+    /**
+     * 
+     * @return Gets resource due date
+     */
     public String getDateDue() {
         return dateDue;
     }
 
-    public void setDateDue(String dateDue) {
-        this.dateDue = dateDue;
-    }
+    /**
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-}
