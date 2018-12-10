653c62aeb4cf40ad65e62d98b88b4b00e424911b
KarlOdukwe <45237187+KarlOdukwe@users.noreply.github.com>
2018-12-10 19:30:56 +0000

    Update CopyHistory.java
    Updated javadoc/formatting

--- src/main/CopyHistory.java
+++ src/main/CopyHistory.java
@@ -10,6 +10,13 @@ public class CopyHistory {
     private String copyReturned;
     private String borrowedBy;
 
+    /**
+     * 
+     * @param copyTitle The title of the copied resource
+     * @param copyBorrowed The time the copy has borrowed
+     * @param copyReturned The time the copy has been returned
+     * @param borrowedBy The time when the copy has been borrowed
+     */
     public CopyHistory(String copyTitle, String copyBorrowed, String copyReturned, String borrowedBy) {
         this.copyTitle = copyTitle;
         this.copyBorrowed = copyBorrowed;
@@ -17,18 +24,30 @@ public class CopyHistory {
         this.borrowedBy = borrowedBy;
     }
 
+    /*
+     * Gets the title of the resource
+     */
     public String getCopyTitle() {
         return copyTitle;
     }
 
+    /*
+     * Gets the copy of the borrowed resource
+     */
     public String getCopyBorrowed() {
         return copyBorrowed;
     }
 
+    /*
+     * Gets the returned copy
+     */
     public String getCopyReturned() {
         return copyReturned;
     }
 
+    /*
+     * Gets the copies borrowed by date
+     */
     public String getBorrowedBy() {
         return borrowedBy;
     }
